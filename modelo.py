1484c7544f73322dd114fb18c3a76b12a0ea2546
murilo <muril0.esteves@protonmail.com>
2023-07-15 23:08:10 -0400

    adicinando classe protocolo

--- modelo.py
+++ modelo.py
@@ -1,9 +1,16 @@
 class Protocolo:
-    def __init__(self, objetivo, series, repeticoes, intervalo):
+    def __init__(self, nivel, objetivo, zona_de_reps, series_por_exercicio, intervalo, velocidade_do_movimento, frequencia_semanal):
+        self._nivel = nivel
         self._objetivo = objetivo
-        self._series = series
-        self._repeticoes = repeticoes
+        self._zona_de_reps = zona_de_reps
+        self._series_por_exercicio = series_por_exercicio
         self._intervalo = intervalo
+        self._velocidade_do_movimento = velocidade_do_movimento
+        self._frequencia_semanal = frequencia_semanal
+    def __str__(self):
+        return f'Nível: {self._nivel}\nObjetivo: {self._objetivo}\nZona de repetições: {self._zona_de_reps}\n' \
+               f'Séries por exercício: {self._series_por_exercicio}\nIntervalo: {self._intervalo}\n' \
+               f'Velocidade do movimento: {self._velocidade_do_movimento}\nFrequência semanal: {self._frequencia_semanal}'
 class Sessao_de_treinamento:
     def __init__(self, nome, exercicios):
         self.nome = nome
@@ -13,16 +20,15 @@ class Sessao_de_treinamento:
     @property
     def lista(self):
         return self._exercicios
-
     def __len__(self):
         return len(self._exercicios)
-
 class Exercicio:
     def __init__(self, nome):
         self._nome = nome
     def __str__(self):
         return f'{self._nome}'
 
+#Área de testes
 #criando exercícios
 agachamento = Exercicio('Agachamento')
 supino = Exercicio('Supino')
@@ -33,6 +39,11 @@ terra = Exercicio('Levantamento terra')
 lista_exercicios_A = [agachamento, supino, barra_fixa, terra]
 treino_A = Sessao_de_treinamento('A', lista_exercicios_A)
 
+iniciante_forca = Protocolo('iniciante', 'hipertrofia', '12 a 15', '3', '1 minuto', 'moderada', '3x na semana')
+
+print(iniciante_forca)
+
+#dinâmica de uma sessão:
 print(f'Treino {treino_A.nome}')
 for exercicio in treino_A:
     print(exercicio)
